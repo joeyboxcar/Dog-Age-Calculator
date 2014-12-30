32c01fe5d56bb764594e68b27a3a670c088f1bfb
joeyboxcar <jtraverso@me.com>
2014-12-30 09:39:05 -0800

    First Commit

--- Dog Age Calculator/ViewController.swift
+++ Dog Age Calculator/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var enterDogAgeTextField: UITextField!
+    @IBOutlet weak var convertedDogAgeLabel: UILabel!
+    @IBOutlet weak var dogYearsTitleLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +24,20 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    @IBAction func dogAgeConverterButton(sender: UIButton) {
+        let humanYearsInputValue = enterDogAgeTextField.text.toInt()!
+        
+        let dogAgeConverter = 7
+        
+        enterDogAgeTextField.resignFirstResponder()
+        
+        dogYearsTitleLabel.hidden = false
+        convertedDogAgeLabel.hidden = false
+        convertedDogAgeLabel.text = "\(humanYearsInputValue * dogAgeConverter)"
+        
+    }
+    
 
 }
 
